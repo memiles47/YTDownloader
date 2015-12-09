17640988228d4de3c371f315727bfb1cbcc44339
Michael Miles <boat081062@gmail.com>
2015-12-09 17:16:23 -0600

    Revised size of completed interface.

--- YouTubeDownloader-01/Form1.Designer.cs
+++ YouTubeDownloader-01/Form1.Designer.cs
@@ -54,10 +54,9 @@
             // pic_TitleBox
             // 
             this.pic_TitleBox.Image = ((System.Drawing.Image)(resources.GetObject("pic_TitleBox.Image")));
-            this.pic_TitleBox.Location = new System.Drawing.Point(616, 15);
-            this.pic_TitleBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pic_TitleBox.Location = new System.Drawing.Point(462, 12);
             this.pic_TitleBox.Name = "pic_TitleBox";
-            this.pic_TitleBox.Size = new System.Drawing.Size(321, 204);
+            this.pic_TitleBox.Size = new System.Drawing.Size(241, 163);
             this.pic_TitleBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pic_TitleBox.TabIndex = 0;
             this.pic_TitleBox.TabStop = false;
@@ -65,30 +64,27 @@
             // lbl_DecorateFolderBox
             // 
             this.lbl_DecorateFolderBox.AutoSize = true;
-            this.lbl_DecorateFolderBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_DecorateFolderBox.Location = new System.Drawing.Point(21, 226);
-            this.lbl_DecorateFolderBox.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_DecorateFolderBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DecorateFolderBox.Location = new System.Drawing.Point(16, 181);
             this.lbl_DecorateFolderBox.Name = "lbl_DecorateFolderBox";
-            this.lbl_DecorateFolderBox.Size = new System.Drawing.Size(150, 25);
+            this.lbl_DecorateFolderBox.Size = new System.Drawing.Size(121, 20);
             this.lbl_DecorateFolderBox.TabIndex = 1;
             this.lbl_DecorateFolderBox.Text = "Download Path:";
             // 
             // tb_DownloadPath
             // 
-            this.tb_DownloadPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_DownloadPath.Location = new System.Drawing.Point(232, 226);
-            this.tb_DownloadPath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_DownloadPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_DownloadPath.Location = new System.Drawing.Point(143, 181);
             this.tb_DownloadPath.Name = "tb_DownloadPath";
-            this.tb_DownloadPath.Size = new System.Drawing.Size(899, 30);
+            this.tb_DownloadPath.Size = new System.Drawing.Size(706, 26);
             this.tb_DownloadPath.TabIndex = 2;
             // 
             // btn_Browse
             // 
-            this.btn_Browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Browse.Location = new System.Drawing.Point(1140, 226);
-            this.btn_Browse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Browse.Location = new System.Drawing.Point(855, 181);
             this.btn_Browse.Name = "btn_Browse";
-            this.btn_Browse.Size = new System.Drawing.Size(133, 39);
+            this.btn_Browse.Size = new System.Drawing.Size(100, 31);
             this.btn_Browse.TabIndex = 3;
             this.btn_Browse.Text = "Browse";
             this.btn_Browse.UseVisualStyleBackColor = true;
@@ -97,11 +93,10 @@
             // chk_OpenAfterDL
             // 
             this.chk_OpenAfterDL.AutoSize = true;
-            this.chk_OpenAfterDL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chk_OpenAfterDL.Location = new System.Drawing.Point(1281, 226);
-            this.chk_OpenAfterDL.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chk_OpenAfterDL.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chk_OpenAfterDL.Location = new System.Drawing.Point(961, 181);
             this.chk_OpenAfterDL.Name = "chk_OpenAfterDL";
-            this.chk_OpenAfterDL.Size = new System.Drawing.Size(225, 29);
+            this.chk_OpenAfterDL.Size = new System.Drawing.Size(188, 24);
             this.chk_OpenAfterDL.TabIndex = 4;
             this.chk_OpenAfterDL.Text = "Open After Download";
             this.chk_OpenAfterDL.UseVisualStyleBackColor = true;
@@ -109,43 +104,39 @@
             // lbl_DecorateYouTubeURLBox
             // 
             this.lbl_DecorateYouTubeURLBox.AutoSize = true;
-            this.lbl_DecorateYouTubeURLBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_DecorateYouTubeURLBox.Location = new System.Drawing.Point(21, 311);
-            this.lbl_DecorateYouTubeURLBox.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_DecorateYouTubeURLBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DecorateYouTubeURLBox.Location = new System.Drawing.Point(16, 249);
             this.lbl_DecorateYouTubeURLBox.Name = "lbl_DecorateYouTubeURLBox";
-            this.lbl_DecorateYouTubeURLBox.Size = new System.Drawing.Size(142, 25);
+            this.lbl_DecorateYouTubeURLBox.Size = new System.Drawing.Size(115, 20);
             this.lbl_DecorateYouTubeURLBox.TabIndex = 5;
             this.lbl_DecorateYouTubeURLBox.Text = "YouTube URL:";
             // 
             // tb_YouTubeURL
             // 
-            this.tb_YouTubeURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_YouTubeURL.Location = new System.Drawing.Point(232, 311);
-            this.tb_YouTubeURL.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tb_YouTubeURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_YouTubeURL.Location = new System.Drawing.Point(143, 249);
             this.tb_YouTubeURL.Name = "tb_YouTubeURL";
-            this.tb_YouTubeURL.Size = new System.Drawing.Size(899, 30);
+            this.tb_YouTubeURL.Size = new System.Drawing.Size(706, 26);
             this.tb_YouTubeURL.TabIndex = 6;
             // 
             // cmb_FileType
             // 
-            this.cmb_FileType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_FileType.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmb_FileType.FormattingEnabled = true;
             this.cmb_FileType.Items.AddRange(new object[] {
             "Video",
             "MP3"});
-            this.cmb_FileType.Location = new System.Drawing.Point(1140, 311);
-            this.cmb_FileType.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmb_FileType.Location = new System.Drawing.Point(855, 249);
             this.cmb_FileType.Name = "cmb_FileType";
-            this.cmb_FileType.Size = new System.Drawing.Size(132, 33);
+            this.cmb_FileType.Size = new System.Drawing.Size(100, 28);
             this.cmb_FileType.TabIndex = 7;
             // 
             // btn_DownLoad
             // 
-            this.btn_DownLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_DownLoad.Location = new System.Drawing.Point(1281, 311);
-            this.btn_DownLoad.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_DownLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DownLoad.Location = new System.Drawing.Point(961, 249);
             this.btn_DownLoad.Name = "btn_DownLoad";
-            this.btn_DownLoad.Size = new System.Drawing.Size(145, 39);
+            this.btn_DownLoad.Size = new System.Drawing.Size(109, 31);
             this.btn_DownLoad.TabIndex = 8;
             this.btn_DownLoad.Text = "DownLoad";
             this.btn_DownLoad.UseVisualStyleBackColor = true;
@@ -154,49 +145,44 @@
             // lbl_DecorateFileDownloading
             // 
             this.lbl_DecorateFileDownloading.AutoSize = true;
-            this.lbl_DecorateFileDownloading.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_DecorateFileDownloading.Location = new System.Drawing.Point(21, 409);
-            this.lbl_DecorateFileDownloading.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_DecorateFileDownloading.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DecorateFileDownloading.Location = new System.Drawing.Point(16, 327);
             this.lbl_DecorateFileDownloading.Name = "lbl_DecorateFileDownloading";
-            this.lbl_DecorateFileDownloading.Size = new System.Drawing.Size(167, 25);
+            this.lbl_DecorateFileDownloading.Size = new System.Drawing.Size(134, 20);
             this.lbl_DecorateFileDownloading.TabIndex = 9;
             this.lbl_DecorateFileDownloading.Text = "File Downloading:";
             // 
             // lbl_FileName
             // 
             this.lbl_FileName.AutoSize = true;
-            this.lbl_FileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_FileName.Location = new System.Drawing.Point(232, 409);
-            this.lbl_FileName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_FileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_FileName.Location = new System.Drawing.Point(174, 327);
             this.lbl_FileName.Name = "lbl_FileName";
-            this.lbl_FileName.Size = new System.Drawing.Size(0, 25);
+            this.lbl_FileName.Size = new System.Drawing.Size(0, 20);
             this.lbl_FileName.TabIndex = 10;
             // 
             // lbl_DecorateDownLoadProgress
             // 
             this.lbl_DecorateDownLoadProgress.AutoSize = true;
-            this.lbl_DecorateDownLoadProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_DecorateDownLoadProgress.Location = new System.Drawing.Point(21, 492);
-            this.lbl_DecorateDownLoadProgress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_DecorateDownLoadProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DecorateDownLoadProgress.Location = new System.Drawing.Point(16, 394);
             this.lbl_DecorateDownLoadProgress.Name = "lbl_DecorateDownLoadProgress";
-            this.lbl_DecorateDownLoadProgress.Size = new System.Drawing.Size(188, 25);
+            this.lbl_DecorateDownLoadProgress.Size = new System.Drawing.Size(151, 20);
             this.lbl_DecorateDownLoadProgress.TabIndex = 11;
             this.lbl_DecorateDownLoadProgress.Text = "Download Progress:";
             // 
             // pg_Download
             // 
-            this.pg_Download.Location = new System.Drawing.Point(232, 492);
-            this.pg_Download.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pg_Download.Location = new System.Drawing.Point(174, 394);
             this.pg_Download.Name = "pg_Download";
-            this.pg_Download.Size = new System.Drawing.Size(900, 30);
+            this.pg_Download.Size = new System.Drawing.Size(675, 24);
             this.pg_Download.TabIndex = 12;
             // 
             // pic_PreviewBox
             // 
-            this.pic_PreviewBox.Location = new System.Drawing.Point(1160, 395);
-            this.pic_PreviewBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pic_PreviewBox.Location = new System.Drawing.Point(870, 316);
             this.pic_PreviewBox.Name = "pic_PreviewBox";
-            this.pic_PreviewBox.Size = new System.Drawing.Size(372, 210);
+            this.pic_PreviewBox.Size = new System.Drawing.Size(279, 168);
             this.pic_PreviewBox.TabIndex = 13;
             this.pic_PreviewBox.TabStop = false;
             // 
@@ -211,9 +197,9 @@
             // 
             // frm_YTDownloader
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1552, 620);
+            this.ClientSize = new System.Drawing.Size(1164, 496);
             this.Controls.Add(this.pic_PreviewBox);
             this.Controls.Add(this.pg_Download);
             this.Controls.Add(this.lbl_DecorateDownLoadProgress);
@@ -228,10 +214,9 @@
             this.Controls.Add(this.tb_DownloadPath);
             this.Controls.Add(this.lbl_DecorateFolderBox);
             this.Controls.Add(this.pic_TitleBox);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm_YTDownloader";
             this.Text = "YouTube Downloader 1.0";
             this.Load += new System.EventHandler(this.Form1_Load);
